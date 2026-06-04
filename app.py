ae06534f04bf95c4a093c7b0b82bf1467fd7c7c6
jorgecif <jorgecif@gmail.com>
2026-06-04 15:10:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 #from langchain.text_splitter import CharacterTextSplitter
 from langchain_text_splitters import CharacterTextSplitter
-#from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
