a3db9be5ff202a34b6903894f50db1bd15d0c089
jorgecif <jorgecif@gmail.com>
2026-06-04 15:54:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from langchain_community.document_loaders import (
     UnstructuredFileLoader,
 )
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain_core.documents import Document
 import pytube
